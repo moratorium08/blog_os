7bdfbd97f8c42df5d3feeb0c1f797ac979b29fbe
moratorium08 <moratorium08@gmail.com>
2018-08-09 14:47:12 +0900

    Blog 1

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+#![feature(panic_implementation)]
+#![no_std]
+#![no_main]
+use core::panic::PanicInfo;
+
+#[panic_implementation]
+#[no_mangle]
+pub fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+// On macOS:
+
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    loop {}
+}
