c1df2d26a238b0f5032227ee61b403fb4665c591
moratorium08 <moratorium08@gmail.com>
2018-08-09 19:53:20 +0900

    Half of Section 3

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 #![feature(panic_implementation)]
+#![feature(exclusive_range_pattern)]
 #![no_std]
 #![no_main]
 
 extern crate bootloader_precompiled;
+extern crate volatile;
+
+mod vga_buffer;
 
 use core::panic::PanicInfo;
 
+use vga_buffer::{Buffer, Writer, Color, ColorCode};
 
 static HELLO: &[u8] = b"Hello World!";
 
@@ -17,14 +22,18 @@ pub fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    use core::fmt::Write;
+    let mut writer = Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    };
+    writer.write_byte(b'H');
+    writer.write_string("ello ");
+    writer.write_string("Wörld!");
+    write!(writer, "The numebrs {}, {}", 42, 1.0 / 3.0).unwrap();
+    writeln!(writer, "hoge");
+    writeln!(writer, "fuga");
 
     loop {}
 }
