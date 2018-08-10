025dbcb04b907c26454a4b726a5d07dfea4809f4
moratorium08 <moratorium08@gmail.com>
2018-08-10 20:44:43 +0900

    Complete section 4

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 #![feature(panic_implementation)]
 #![feature(exclusive_range_pattern)]
 #![no_std]
-#![no_main]
+#![cfg_attr(not(test), no_main)]
+#![cfg_attr(test, allow(dead_code, unused_macros, unused_imports))]
 
 extern crate bootloader_precompiled;
 extern crate spin;
 extern crate volatile;
 #[macro_use]
 extern crate lazy_static;
+#[cfg(test)]
+extern crate std;
+#[cfg(test)]
+extern crate array_init;
+
 
 #[macro_use]
 mod vga_buffer;
@@ -18,6 +24,7 @@ use vga_buffer::{Buffer, Writer, Color, ColorCode};
 
 static HELLO: &[u8] = b"Hello World!";
 
+#[cfg(not(test))]
 #[panic_implementation]
 #[no_mangle]
 pub fn panic(info: &PanicInfo) -> ! {
@@ -25,6 +32,7 @@ pub fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
+#[cfg(not(test))]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     use core::fmt::Write;
